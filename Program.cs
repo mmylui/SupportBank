564eb7022d0ba08f279d1eb387e585cccb5cf86b
unknown <paul7markets@hotmail.com>
2023-08-10 13:32:15 +0100

    logging using Variable file path

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using NLog.Config;
 using NLog.Targets;
 
 var config = new LoggingConfiguration();
-var target = new FileTarget { FileName = @"/Users/mina/Documents/Software Development/TechSwitch/Training/SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
+var target = new FileTarget { FileName = Variables.loggingPath, Layout = @"${longdate} ${level} - ${logger}: ${message}" };
 config.AddTarget("File Logger", target);
 config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
 LogManager.Configuration = config;
